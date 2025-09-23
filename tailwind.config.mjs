5c95e06a55bdd78a1b2151f8d11496461c1f9df9
eldritchtools <eldritch.tools@gmail.com>
2025-09-23 16:14:26 +0800

    Remove next export

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,6 @@ const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
